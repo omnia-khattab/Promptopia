7af077555b8a2ee8673b636ea49db95c46ee367a
omniak <omniakhattab17@gmail.com>
2024-09-28 01:34:44 +0300

    Disable caching

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,10 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt");
+    
+    const response = await fetch("/api/prompt",{
+      cache: "no-cache", // Disable caching
+    });
     const data = await response.json();
 
     setAllPosts(data);
